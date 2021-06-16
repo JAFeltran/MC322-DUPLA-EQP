b9cb0e12d48347f51a1a1f78d09bab385cc4b685
Fábio de Andrade Barboza <barbozafab@outlook.com>
2021-06-16 18:06:14 -0300

    Update IHeroi

--- Projeto/src/src/Ator/IHeroi.java
+++ Projeto/src/src/Ator/IHeroi.java
@@ -1,7 +1,5 @@
 package src.Ator;
 
-import src.Mapa.IMapa;
-
 public interface IHeroi extends IAtor, ICombate, IAcao {
     // Setters
     public void setItemInventario(int posicao, int valor, String nome);
